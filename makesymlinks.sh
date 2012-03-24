3da6af398d4849c0450b964d74618e71b66ef250
Michael J. Smalley <michaeljsmalley@gmail.com>
2012-03-24 00:32:56 -0400

    Added comments to setup script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -31,20 +31,24 @@ for file in $files; do
 done
 
 function install_zsh {
-# Test to see if zshell is installed
+# Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
         git clone http://github.com/michaeljsmalley/oh-my-zsh.git
     fi
+    # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
     fi
 else
+    # If zsh isn't installed, get the platform of the current machine
     platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
         sudo apt-get install zsh
         install_zsh
+    # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
         echo "Please install zsh, then re-run this script!"
         exit
