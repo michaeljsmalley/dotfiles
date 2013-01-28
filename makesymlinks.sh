d1394a90600894851b6eaf6a4e88d8b75e6c69ed
Mingliang Liu <liuml07@gmail.com>
2013-01-28 19:22:39 +0800

    Make the echo message even more good-looking

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,14 +13,14 @@ files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # li
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "...done"
+echo "done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "...done"
+echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
