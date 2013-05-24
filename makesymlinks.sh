7381500e300b0e56658df657dbb09d8862d74ba1
Tim <timbinous@gmail.com>
2013-05-23 21:40:14 -0400

    using pwd of the cloned dotfiles rather than a specific location

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=`pwd`
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
 
