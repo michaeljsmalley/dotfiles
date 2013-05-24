1858c888f551fdffa6ee8c3bd87eb65fc10a674a
Tim <timbinous@gmail.com>
2013-05-23 21:45:07 -0400

    couldnt see the purpose of changing to the  directory

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -17,11 +17,6 @@ echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
