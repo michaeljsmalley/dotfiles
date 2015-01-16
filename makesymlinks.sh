9b94aa955724dc3ef8dcdb68687862a7b62abcd3
Jhonny Moreira <xDeniedAccess@gmail.com>
2015-01-16 04:18:56 -0200

    From path to variable.
    Changed the '~/dotfiles_old' on line 28 to the variable created before '$olddir'.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -25,7 +25,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
